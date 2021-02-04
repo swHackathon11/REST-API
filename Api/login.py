cc5c8dd214b435e829d5cb591172f005c9f159d1
heewoo0419 <heewoo0419@gmail.com>
2021-02-04 17:09:41 +0900

    Test Heroku

---
+++ Api/login.py
@@ -0,0 +1,20 @@
+import json
+from flask_restplus import Namespace, fields, Resource, reqparse
+
+Login = Namespace('login', description='로그인')
+
+model_login = Login.model('login data', {
+    'user_id': fields.String(description='블랙보드 ID', required=True),
+    'user_password': fields.String(description='블랙보드 Password', required=True)
+})
+
+
+@Login.route('')
+class PostLogin(Resource):
+    @Login.expect(model_login)
+    @Login.response(200, 'OK')
+    @Login.response(400, 'Bad Request')
+    @Login.response(401, 'Unauthorized')
+    @Login.response(500, 'Internal Server Error')
+    def post(self):
+        return 'Login'
--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from flask_restplus import Api
+from flask_cors import CORS
+from Api.login import Login
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['JSON_AS_ASCII'] = False
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+api = Api(app, version='1.0', title='알바꼼꼼 REST API', description='알바꼼꼼 백엔드 REST API 입니다!')
+
+ns_login = api.namespace('/', description='로그인')
 
 
 @app.route('/')
 def main():
     return 'Hello, World!'
 
+api.add_namespace(Login, '/login')                  # 로그인
 
 if __name__ == '__main__':
     app.run(debug=True)
