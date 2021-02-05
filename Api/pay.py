f1b6e8c393631cad71e8ac0f691b7437e22c491e
heewoo0419 <heewoo0419@gmail.com>
2021-02-06 00:00:55 +0900

    급여 월단위 조회 기능 추가

--- Api/pay.py
+++ Api/pay.py
@@ -12,6 +12,8 @@ model_pay = Pay.model('Pay Data', {
     'token': fields.String(description='로그인 토큰', required=True),
     'workplace_id': fields.String(description='매장 아이디', required=True),
     'employee_id': fields.String(description='알바 아이디', required=True),
+    'year': fields.Integer(description='Year', required=True),
+    'month': fields.Integer(description='Month', required=True),
 })
 
 
@@ -28,11 +30,16 @@ class PostLogin(Resource):
         __parser.add_argument('token', type=str)
         __parser.add_argument('workplace_id', type=str)
         __parser.add_argument('employee_id', type=str)
+        __parser.add_argument('year', type=int)
+        __parser.add_argument('month', type=int)
         __args = __parser.parse_args()
 
         __token = __args['token']
         __workplace_id = __args['workplace_id']
         __employee_id = __args['employee_id']
+        __year = __args['year']
+        __month = __args['month']
+
 
         try:
             __auth = jwt.decode(__token, JWT["key"], algorithms="HS256")
@@ -48,9 +55,14 @@ class PostLogin(Resource):
 
         cursor = alba_db.cursor(pymysql.cursors.DictCursor)
         query = 'select * from workplace_schedule ' \
-                'where workplace_id = "{workplace_id}" and employee_id = "{employee_id}" and is_checked = 2;'
+                'where workplace_id = "{workplace_id}" and employee_id = "{employee_id}" ' \
+                'and Year(date) = {year} and Month(date) = {month} and is_checked = 2;'
 
-        cursor.execute(query.format(workplace_id=__workplace_id, employee_id=__employee_id))
+        cursor.execute(query.format(workplace_id=__workplace_id,
+                                    employee_id=__employee_id,
+                                    year=__year,
+                                    month=__month
+                                    ))
         __result = cursor.fetchall()
 
         def default(o):
